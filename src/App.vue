38267c0a3e57e9839ff44d2e7262b78de0cdcf38
Scorpio <18556134754@163.com>
2022-06-01 18:32:08 +0800

    some update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <div id="app">
-    <vue-super-tree></vue-super-tree>
+    <vue-super-tree :tree="treeData" valueKey="_key" :height="height">
+      <template v-slot="{ item }">
+        <span>{{ item._key }}</span>
+      </template>
+    </vue-super-tree>
   </div>
 </template>
 
 <script>
+import treeData from "./api/index";
+
 export default {
   name: "App",
+  data() {
+    return {
+      treeData,
+      height: 0,
+    };
+  },
+  created() {
+    this.height = document.documentElement.clientHeight - 16;
+  },
 };
 </script>
---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+function recursionGenerateTree(level, range, depth, prefix) {
+  const arr = [];
+  for (let i = 1; i <= range; i++) {
+    const item = {
+      _key: prefix ? `${prefix}-${i}` : `${i}`,
+    };
+    if (level >= depth) {
+      item.children = [];
+    } else {
+      item.children = recursionGenerateTree(level + 1, range, depth, item._key);
+    }
+    arr.push(item);
+  }
+  return arr;
+}
+
+export default recursionGenerateTree(1, 24, 3);
