3e45da338dfed2669ca4e42d5d3cf100f6564898
白夜 <18556134754@163.com>
2022-06-05 18:09:02 +0800

    some update

--- deploy/.vuepress/config.js
+++ deploy/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  base: process.env.NODE_ENV === "production" ? "/vue-super-tree" : "/",
+  base: process.env.NODE_ENV === "production" ? "/vue-super-tree/" : "/",
   dest: "docs",
   head: [
     ['link', {
